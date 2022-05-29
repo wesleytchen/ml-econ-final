9629b78fceb638b496dab953b9120e4d5fdbadf5
hannhat <96318064+hannhat@users.noreply.github.com>
2022-05-29 16:55:58 -0500

    stash

--- code/model.py
+++ code/model.py
@@ -11,19 +11,19 @@ parent_path = str(finpath) + '/'
 final_df = pd.read_csv(parent_path + "FinalDF.csv")
 adj_counties = pd.read_csv(parent_path + "AdjacentDelta.csv")
 
-final_df['Presence of Water Violation'] = final_df['Presence of Water Violation'].str.replace('Yes','1')
-final_df['Presence of Water Violation'] = final_df['Presence of Water Violation'].str.replace('No','0')
-final_df['Presence of Water Violation'] = pd.to_numeric(final_df['Presence of Water Violation'])
-
-population = final_df["Population"] / final_df["Population"].max()
+population = np.log(final_df["Population"]) / np.log(final_df["Population"].max())
 print(population)
 final_df = final_df.select_dtypes(exclude=['object'])
-features_df = final_df.drop(columns=["% Smokers", "Population", "Life Expectancy"])
-scaler = StandardScaler()
-scaler.fit(features_df)
-standardized_df = scaler.transform(features_df)
-#print(standardized_df)
+final_df = final_df.drop(columns=["Population", "Life Expectancy"])
+feature_cols = final_df.columns
+std_final_df = pd.DataFrame(StandardScaler().fit_transform(final_df), columns=feature_cols)
+std_final_df["Population"] = population
+#scaler = StandardScaler()
+#scaler.fit(features_df)
+#std_features_array = scaler.transform(features_df)
 #print(np.shape(standardized_df))
+print(std_final_df)
+
 
 median_income = final_df["Median Household Income"].median()
 df_upper_income = final_df[final_df["Median Household Income"] >= median_income]
