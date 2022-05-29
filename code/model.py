ec2e4deac1e4ac37b5d3ae0b9b81163ba10b97e5
hannhat <96318064+hannhat@users.noreply.github.com>
2022-05-29 18:00:04 -0500

    stash

--- code/model.py
+++ code/model.py
@@ -10,38 +10,52 @@ parent_path = str(finpath) + '/'
 
 final_df = pd.read_csv(parent_path + "FinalDF.csv")
 adj_counties = pd.read_csv(parent_path + "AdjacentDelta.csv")
+final_df["Population"] = np.sqrt(final_df["Population"]) / np.sqrt(final_df["Population"].max()) #Ask about specific transformation for weighting to use here
 
-population = np.log(final_df["Population"]) / np.log(final_df["Population"].max())
-print(population)
-final_df = final_df.select_dtypes(exclude=['object'])
-final_df = final_df.drop(columns=["Population", "Life Expectancy"])
-feature_cols = final_df.columns
-std_final_df = pd.DataFrame(StandardScaler().fit_transform(final_df), columns=feature_cols)
-std_final_df["Population"] = population
-#scaler = StandardScaler()
-#scaler.fit(features_df)
-#std_features_array = scaler.transform(features_df)
-#print(np.shape(standardized_df))
-print(std_final_df)
+def standardize_df(df):
+    df = df.select_dtypes(exclude=['object'])
+    df = df.drop(columns=["Life Expectancy"])
+    feature_cols = df.columns[2:]
+    std_final_df = pd.DataFrame(StandardScaler().fit_transform(df.iloc[:, 2:]), columns=feature_cols)
+    pop_df = df["Population"]
+    return std_final_df, pop_df
 
 
 median_income = final_df["Median Household Income"].median()
 df_upper_income = final_df[final_df["Median Household Income"] >= median_income]
 df_lower_income = final_df[final_df["Median Household Income"] < median_income]
+#print(df_upper_income["Population"])
+#print(df_lower_income.shape)
 
+# Here, our dataframes are not the same shapes because % Completed High School has many values at the median,
+# but the number of observations in the dataframes are 1197 for the upper bracket and 1209 for the lower bracket,
+# which we found to be acceptable for running our regression analysis on
 median_edu = final_df["% Completed High School"].median()
-df_upper_edu = final_df[final_df["% Completed High School"] >= median_edu]
-df_lower_edu = final_df[final_df["% Completed High School"] < median_edu]
-
-df_lst = [df_upper_income, df_lower_income, df_upper_edu, df_lower_edu]
+df_upper_edu = final_df[final_df["% Completed High School"] > median_edu]
+df_lower_edu = final_df[final_df["% Completed High School"] <= median_edu]
 
 # Adjacent counties regression
 
 adj_counties = adj_counties.select_dtypes(exclude=['object'])
-features_df2 = adj_counties.drop(columns=["% Smokers", "Life Expectancy"])
-scaler = StandardScaler()
-scaler.fit(features_df2)
-standardized_df2 = scaler.transform(features_df2)
-#print(standardized_df2)
+adj_counties = adj_counties.drop(columns=["Life Expectancy"])
+
+
+# Standardizing values and creating all the matrices we need to run Lasso
+
+df_lst = [final_df, df_upper_income, df_lower_income, df_upper_edu, df_lower_edu] #Want to add adj_counties to this list after Population column is added
+params_lst = []
+for frame in df_lst:
+    std_frame, pop_df = standardize_df(frame)
+    label_df = std_frame["% Smokers"]
+    features_df = std_frame.drop(columns=["% Smokers"])
+    pop_weights = np.array(pop_df)
+    X = np.array(features_df)
+    y = np.array(label_df)
+    values = [X, y, pop_weights]
+    params_lst.append(values)
+#print(params_lst)
+
+
+
 
 
